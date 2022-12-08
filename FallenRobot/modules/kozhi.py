852c993865c9caab495ed58bfcee5df61a98df07
SOUL々H҉A҉C҉K҉E҉R҉ <63580147+dqanshi@users.noreply.github.com>
2022-12-08 19:10:47 +0530

    Create kozhi.py

---
+++ FallenRobot/modules/kozhi.py
@@ -0,0 +1,46 @@
+import html
+import random
+import time
+from SiestaRobot import dispatcher
+from SiestaRobot.modules.disable import DisableAbleCommandHandler, DisableAbleMessageHandler
+from telegram.error import BadRequest
+from telegram.ext import CallbackContext ,Filters,CommandHandler,run_async
+
+KOYI_STRINGS = (
+     "കുളക്കോഴി🐔 ", 
+     "നാടൻ കോഴി🐔🐔 ", 
+     "ഗിരിരാജൻ കോഴി 🐓🐓", 
+     "🐔🐔കൂട്ടിൽ കയറു കോഴി ", 
+     "🐓🐓പ്ഫാ കോഴി ",
+     "🐔കോഴി സ്പോട്ടേഡ് 🐔",
+     "🐔🐓ഇവിടെ രണ്ട് പൊരിച്ച കോഴി ",
+     "🐓🐓ഉളുപ്പ് ഉണ്ടോ കോഴിയെ ",
+     "🐔ഡാ മോനെ കോഴി അത് ലോക്കാ..ഇങ് പോര് ",
+     "🐔🐔എന്താ കോഴിമോനെ ",
+     "🐓🐓 എന്താടാ നോക്കുനെ കോഴിയെ."
+     "😂വിധി പോലും വിറച്ചുപ്പോയി ഇവന്റെ കോഴിത്തരം കണ്ട് 🐓🐔🐓",
+     " എടാ കോഴി നിനക്ക് ഇതുതന്നെ ആണോ പണി 🐓🤭",
+     "😏നിന്റെ ഒകെ കോഴിത്തുക്കല്മുളക്കുന്ന കാലത്ത് നുമ്മ ഈ സീൻ വിട്ടതാ",
+     "നീ കോഴി ആണോ അതോ കോഴി ആയി അഭിനയിക്കുവാണോ🤭🤣🤣🐓",
+     
+)
+
+@run_async
+def kozhi(update, context):
+    # reply to correct message
+    reply_text = (
+        update.effective_message.reply_to_message.reply_text
+        if update.effective_message.reply_to_message
+        else update.effective_message.reply_text
+    )
+    reply_text(random.choice(KOYI_STRINGS))
+
+
+
+
+
+__mod_name__ = "kozhi "
+
+KOZHI_HANDLER = DisableAbleCommandHandler("kozhi", kozhi)
+
+dispatcher.add_handler(KOZHI_HANDLER)

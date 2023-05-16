3e10f440be9e0adb2dd459c8277be216a7608485
SOUL々H҉A҉C҉K҉E҉R҉ <63580147+dqanshi@users.noreply.github.com>
2023-05-16 10:31:52 +0530

    Update Antiarabic.py

--- FallenRobot/modules/Antiarabic.py
+++ FallenRobot/modules/Antiarabic.py
@@ -6,7 +6,7 @@ from telegram.utils.helpers import mention_html
 from FallenRobot import dispatcher, CallbackContext, LOGGER
 from FallenRobot import DRAGONS as SUDO_USERS
 from FallenRobot.modules.helper_funcs.chat_status import user_not_admin, user_admin, can_delete, is_user_admin, bot_admin
-from FallenRobot import loggable
+from FallenRobot.log_channel import loggable
 from FallenRobot.modules.helper_funcs.extraction import extract_text
 from FallenRobot.modules.sql import antiarabic_sql as sql
 

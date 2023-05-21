7deed7d2c6e038032dd0ffffaf05eebe3a992e81
SOUL々H҉A҉C҉K҉E҉R҉ <63580147+dqanshi@users.noreply.github.com>
2023-05-21 11:24:35 +0530

    Update mongo.py

--- FallenRobot/utils/mongo.py
+++ FallenRobot/utils/mongo.py
@@ -1,12 +1,19 @@
 from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
 
+import codecs
+import pickle
+from typing import Dict, List, Union
+
+
+
 from FallenRobot import MONGO_DB_URI
 
 mongo = MongoCli(MONGO_DB_URI)
 db = mongo.FallenRobot
 
 coupledb = db.couple
-
+chatsdb = db.chats
+usersdb = db.users
 
 async def _get_lovers(chat_id: int):
     lovers = await coupledb.find_one({"chat_id": chat_id})
@@ -33,3 +40,57 @@ async def save_couple(chat_id: int, date: str, couple: dict):
         {"$set": {"couple": lovers}},
         upsert=True,
     )
+
+    
+    
+    async def is_served_chat(chat_id: int) -> bool:
+    chat = await chatsdb.find_one({"chat_id": chat_id})
+    return bool(chat)
+
+
+async def get_served_chats() -> list:
+    chats = chatsdb.find({"chat_id": {"$lt": 0}})
+    if not chats:
+        return []
+    chats_list = []
+    for chat in await chats.to_list(length=1000000000):
+        chats_list.append(chat)
+    return chats_list
+
+
+async def add_served_chat(chat_id: int):
+    is_served = await is_served_chat(chat_id)
+    if is_served:
+        return
+    return await chatsdb.insert_one({"chat_id": chat_id})
+
+
+async def remove_served_chat(chat_id: int):
+    is_served = await is_served_chat(chat_id)
+    if not is_served:
+        return
+    return await chatsdb.delete_one({"chat_id": chat_id})
+
+
+async def is_served_user(user_id: int) -> bool:
+    user = await usersdb.find_one({"user_id": user_id})
+    return bool(user)
+
+
+async def get_served_users() -> list:
+    users = usersdb.find({"user_id": {"$gt": 0}})
+    if not users:
+        return []
+    users_list = []
+    for user in await users.to_list(length=1000000000):
+        users_list.append(user)
+    return users_list
+
+
+async def add_served_user(user_id: int):
+    is_served = await is_served_user(user_id)
+    if is_served:
+        return
+    return await usersdb.insert_one({"user_id": user_id})
+
+    
